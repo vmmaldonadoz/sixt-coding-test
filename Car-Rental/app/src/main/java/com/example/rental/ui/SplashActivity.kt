02c2b3eeb2e8c8e566080775703634ba81a6b487
Victor Maldonado <vmmaldonadoz@users.noreply.github.com>
2019-12-04 18:40:11 -0500

    Added Splash activity.

---
+++ Car-Rental/app/src/main/java/com/example/rental/ui/SplashActivity.kt
@@ -0,0 +1,42 @@
+package com.example.rental.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rental.R
+import java.util.concurrent.TimeUnit
+
+
+class SplashActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+
+    private val runnable = { startNextActivity() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startNextFlow()
+    }
+
+    private fun startNextFlow() {
+        handler.postDelayed(runnable, TimeUnit.SECONDS.toMillis(2))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+
+    private fun startNextActivity() {
+        val newIntent = Intent(this, MainActivity::class.java)
+        startActivity(newIntent)
+        finish()
+    }
+
+}
